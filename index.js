6583d3ba42be8c08a11eb359af561259eb421bdd
Dracohydrus <dracokris123@gmail.com>
2022-01-16 16:57:44 -0500

    removing cors from project dependencies

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
-const cors = require('cors')
 
 const authRoute = require('./routes/auth');
 const userRoute = require('./routes/users');
@@ -10,7 +9,6 @@ const transactionRoute = require('./routes/transactions');
 
 dotenv.config();
 app.use(express.json());
-app.use(cors());
 
 mongoose.connect(process.env.MONGO_URL) 
 .then(console.log("Connected to MongoDB"))
