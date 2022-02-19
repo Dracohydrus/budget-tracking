058e694475b2baa53a6e9029632fe68c89d1df02
Dracohydrus <dracokris123@gmail.com>
2022-02-19 14:03:53 -0500

    Adding uploading transactions to local storage

--- client/src/components/upload/UploadComponent.jsx
+++ client/src/components/upload/UploadComponent.jsx
@@ -1,18 +1,54 @@
 import './UploadComponent.css'
-import { useRef } from "react"
+import { useEffect, useRef, useState } from "react"
+import { axiosInstance } from '../../config'
 
-const UploadComponent = ({ transaction = {}, onDelete }) => {
-  const { key, description, currency, value, categories, transactionDate } = transaction
+const UploadComponent = ({ transaction = {}, onDelete, onUpdate }) => {
+  const { key, description, currency, value = 0.0, categories, transactionDate } = transaction
+
+  const [categoryList, setCategoryList] = useState([]);
+
   const descriptionRef = useRef();
   const currencyRef = useRef();
   const valueRef = useRef();
-  const calendarRef = useRef();
+  const transactionDateRef = useRef();
+  const categoriesRef = useRef();
+
+  useEffect(() => {
+    descriptionRef.current.value = description
+    currencyRef.current.value = currency
+    valueRef.current.value = value
+    transactionDateRef.current.value = transactionDate
+    categoriesRef.current.value = (categories.length >= 1 && categories[0]) || '';
+  }, [transaction])
+
+  useEffect(() => {
+    axiosInstance.get('/category')
+      .then((categories) => setCategoryList(categories.data))
+      .catch((err) => console.log(err))
+  }, [])
+
+  const onChange = () => {
+    console.log(descriptionRef.current.value)
+    onUpdate(key, {
+      ...transaction,
+      description: descriptionRef.current.value,
+      currency: currencyRef.current.value,
+      value: valueRef.current.value,
+      transactionDate: transactionDateRef.current.value,
+      categories: [categoriesRef.current.value]
+    })
+  }
+
   return (
     <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', gap: '10px' }}>
-      <input type="text" placeholder="Description" ref={descriptionRef} />
-      <input type="text" placeholder="Currency" ref={currencyRef} />
-      <input type="number" placeholder="Value" ref={valueRef} />
-      <input type="date" placeholder="Date" ref={calendarRef} />
+      <input type="text" placeholder="Description" ref={descriptionRef} onChange={onChange} />
+      <input type="text" placeholder="Currency" ref={currencyRef} onChange={onChange} />
+      <input type="number" placeholder="Value" ref={valueRef} onChange={onChange} />
+      <input type="date" placeholder="Date" ref={transactionDateRef} onChange={onChange} />
+      <input list="categoryList" type="text" ref={categoriesRef} onChange={onChange} />
+      <datalist id="categoryList">
+        {categoryList.map((cat) => <option key={cat?._id} value={cat?.name} />)}
+      </datalist>
       <button className='deleteButton' onClick={() => onDelete(key)}><i className="fa-solid fa-multiply"></i></button>
     </div>
   )
--- client/src/pages/upload/Upload.jsx
+++ client/src/pages/upload/Upload.jsx
@@ -1,11 +1,26 @@
 import './Upload.css'
-import { useState } from "react"
-import UploadComponent from "../../components/upload/UploadComponent"
+import { useContext, useEffect, useState } from "react"
+import { Context } from '../../context/Context'
 import { axiosInstance } from "../../config"
 import { toastInstance } from "../../helpers/toast"
+import dateFormat from 'dateformat'
+import UploadComponent from "../../components/upload/UploadComponent"
 
 const Upload = () => {
   const [transactions, setTransactions] = useState([]);
+  const {user} = useContext(Context)
+  
+  useEffect(() => {
+    let storage = JSON.parse(localStorage.getItem('uploadTransactions'))
+    if(!storage) return
+    setTransactions(storage)
+  }, [])
+
+  useEffect(() => {
+    let storage = JSON.stringify(transactions)
+    if(!storage) return
+    localStorage.setItem('uploadTransactions', storage)
+  }, [transactions])
 
   const getLargest = () => {
     var highest = 0;
@@ -19,20 +34,38 @@ const Upload = () => {
     setTransactions(transactions.filter((transaction) => transaction.key !== key))
   }
 
-  const saveTransactions = () => {
-    console.log(transactions)
-    // axiosInstance.post('/transactions', { data: transactions })
-    //   .then((transactions) => {
-    //     setTransactions([])
-    //     toastInstance.success("Transactions Created")
-    //   })
-    //   .catch((err) => toastInstance.error(err))
+  const updateTransaction = (key, newTransaction) => {
+    setTransactions(transactions.map((transaction) => {
+      if(transaction.key === key) return {...transaction, ...newTransaction}
+      return transaction
+    }))
+  }
+
+  const createTransaction = () => {
+    let newTransaction = { 
+      key: getLargest() + 1,
+      description: '',
+      currency: user.currency || 'CAD',
+      value: 0,
+      categories: [],
+      transactionDate: dateFormat(new Date(), 'yyyy-mm-dd') 
+    }
+    setTransactions([...transactions, newTransaction])
+  } 
+
+  const saveTransactions = async () => {
+    axiosInstance.post('/transactions', { data: transactions })
+      .then((transactions) => {
+        setTransactions([])
+        toastInstance.success("Transactions Created")
+      })
+      .catch((err) => toastInstance.error(err))
   }
 
   return (
     <div>
       <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', gap: '5px' }}>
-        {transactions.map((transaction) => <UploadComponent key={transaction.key} transaction={transaction} onDelete={deleteTransaction} />)}
+        {transactions.map((transaction) => <UploadComponent key={transaction.key} transaction={transaction} onDelete={deleteTransaction} onUpdate={updateTransaction} />)}
       </div>
       <div style={{ display: 'flex', justifyContent: 'center', alignContent: 'center', marginTop: '1rem', gap: '5px' }}>
         <button className='uploadButton'>
@@ -41,9 +74,7 @@ const Upload = () => {
         <button className='uploadButton' onClick={() => saveTransactions()}>
           <i className="fa-solid fa-floppy-disk"></i>
         </button>
-        <button className='uploadButton' onClick={() => {
-          setTransactions([...transactions, { key: getLargest() + 1 }])
-        }}>
+        <button className='uploadButton' onClick={createTransaction} >
           <i className="fa-solid fa-plus"></i>
         </button>
       </div>
