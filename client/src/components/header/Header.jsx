1ab40b438893daebe1f9d7acb97a9692674fc77a
hailstormnation <97800580+hailstormnation@users.noreply.github.com>
2022-02-23 21:05:53 -0500

    Plant change (#12)
    * Plant change
    
    Made the banner pic better.
    
    * Update Header.jsx
    
    - Removing comments
    
    Co-authored-by: Dracohydrus <dracokris123@gmail.com>

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,18 +1,18 @@
 import './Header.css'
 import { useContext } from 'react';
 import { Context } from '../../context/Context';
+import bannerImage from '../../assets/images/money-growth.jpg';
 
 const Header = () => {
-  const {user} = useContext(Context);
-
+  const { user } = useContext(Context);
   return (
-      <div className="header">
-          <div className="headerTitles">
-            <span className='headerTitleSmall'>{user ? `Hello ${user.username}` : "React & Node"}</span>
-            <span className='headerTitleLarge'>Budget App</span>
-          </div>
-          <img className='headerImage' src="https://wallsdesk.com/wp-content/uploads/2016/12/Rocky-Mountains-High-Definition-Wallpapers.jpg" alt="" />
+    <div className="header">
+      <div className="headerTitles">
+        <span className='headerTitleSmall'>{user ? `Hello ${user.username}` : "React & Node"}</span>
+        <span className='headerTitleLarge'>Budget App</span>
       </div>
+      <img className='headerImage' src={bannerImage} alt="" />
+    </div>
   )
 };
 
