c128a503de7d420ffc2053fc391f2583da324f2d
Dracohydrus <dracokris123@gmail.com>
2022-02-23 21:05:19 -0500

    Update Header.jsx
    - Removing comments

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -4,17 +4,15 @@ import { Context } from '../../context/Context';
 import bannerImage from '../../assets/images/money-growth.jpg';
 
 const Header = () => {
-  const {user} = useContext(Context);
-
+  const { user } = useContext(Context);
   return (
-      <div className="header">
-          <div className="headerTitles">
-            <span className='headerTitleSmall'>{user ? `Hello ${user.username}` : "React & Node"}</span>
-            <span className='headerTitleLarge'>Budget App</span>
-          </div>
-          <img className='headerImage' src={bannerImage} alt="" />
-          {/* <img className='headerImage' src="https://pixabay.com/get/g7574debd4a7b44f1e10e0b06bd9583c131233286d4fc89ae03009367fd5e08f49df6d78b2e92d4a911c5eaf3d1d16839a588d25ede7c13695548715e0273a32376baca2e8ddb7c74c6911f5b2f76195b_1920.jpg" alt="" /> */}
+    <div className="header">
+      <div className="headerTitles">
+        <span className='headerTitleSmall'>{user ? `Hello ${user.username}` : "React & Node"}</span>
+        <span className='headerTitleLarge'>Budget App</span>
       </div>
+      <img className='headerImage' src={bannerImage} alt="" />
+    </div>
   )
 };
 
