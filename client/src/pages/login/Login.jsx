50d4e5fb7e3a7ce7afe581c1fa19ee1e1e91d62a
Dracohydrus <dracokris123@gmail.com>
2022-02-26 16:33:33 -0500

    Kris background images (#13)
    * Adding Login and Register Backgrounds
    
    - added background images for login and register
    - cleaned up some code for new formatting set up
    
    * Update Transaction.css
    
    - small change on the gap for the transaction categories

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import { axiosInstance } from "../../config";
 import { Context } from "../../context/Context";
 import { LoginStart, LoginSuccessful, LoginFailure } from "../../context/Actions";
+import loginBackground from '../../assets/images/login-background.jpg'
 
 const Login = () => {
   const emailRef = useRef();
@@ -21,9 +22,7 @@ const Login = () => {
         email: emailRef.current.value,
         password: passwordRef.current.value,
       })
-      .then((res) => {
-        dispatch(LoginSuccessful(res.data))
-      })
+      .then((res) => dispatch(LoginSuccessful(res.data)))
       .catch((err) => {
         setError(true);
         dispatch(LoginFailure())
@@ -31,37 +30,16 @@ const Login = () => {
   };
 
   return (
-    <div className="login">
+    <div className="login" style={{ backgroundImage: `url(${loginBackground})` }}>
       <span className="loginTitle">Login</span>
-      <span
-        className="loginError"
-        style={{
-          color: "red",
-          fontSize: "16pt",
-          fontWeight: "bold",
-          marginTop: "5px",
-        }}
-      >
+      <span className="loginError" style={{ color: "red", fontSize: "16pt", fontWeight: "bold", marginTop: "5px", }}>
         {error && "Invalid Credentials"}
       </span>
       <form onSubmit={onSubmit} className="loginForm">
         <label>Email</label>
-        <input
-          className="loginInput"
-          type="text"
-          placeholder="Email"
-          ref={emailRef}
-          data-cy="email"
-          autoFocus
-        />
+        <input className="loginInput" type="text" placeholder="Email" ref={emailRef} data-cy="email" autoFocus />
         <label>Password</label>
-        <input
-          className="loginInput"
-          type="password"
-          placeholder="Password"
-          ref={passwordRef}
-          data-cy="password"
-        />
+        <input className="loginInput" type="password" placeholder="Password" ref={passwordRef} data-cy="password" />
         <button type="submit" className="loginButton" disabled={isFetching} data-cy="login-button">
           Login
         </button>
