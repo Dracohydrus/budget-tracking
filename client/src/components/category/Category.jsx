604d8fd6bf940853f1129353435607140c8203c5
Dracohydrus <dracokris123@gmail.com>
2022-03-02 20:14:52 -0500

    Refactoring Category
    - Removing the requirement for CSS file for Category component

--- client/src/components/category/Category.jsx
+++ client/src/components/category/Category.jsx
@@ -1,7 +1,7 @@
 import { axiosInstance } from "../../config";
 import { toastInstance } from "../../utils/toast";
 import Icon from '../basic/Icon';
-import "./Category.css";
+import styled from 'styled-components';
 
 const Category = ({ category }) => {
   const { _id: id, name } = category;
@@ -9,24 +9,68 @@ const Category = ({ category }) => {
   const deleteCategory = async (id) => {
     axiosInstance
       .delete("/category/" + id)
-      .then((res) => {
-        toastInstance.success("Category Deleted")
-      })
+      .then((res) => toastInstance.success("Category Deleted"))
       .catch((err) => toastInstance.error("Category Not Deleted"));
   };
 
   return (
-    <div className="category">
-      <div className="categoryWrapper">
-        <div className="categoryLeft">
-          <div className="categoryItem">{name}</div>
-        </div>
-        <div className="categoryRight">
-          <Icon className="categoryDeleteIcon fas fa-trash-alt" onClick={() => deleteCategory(id)} />
-        </div>
-      </div>
-    </div>
+    <CategoryDiv>
+      <CategoryWrapperDiv>
+        <CategoryLeftDiv>
+          {name}
+        </CategoryLeftDiv>
+        <CategoryRightDiv>
+          <DeleteIcon className="fas fa-trash-alt" onClick={() => deleteCategory(id)} />
+        </CategoryRightDiv>
+      </CategoryWrapperDiv>
+    </CategoryDiv>
   );
 };
 
+const CategoryDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`
+
+const CategoryWrapperDiv = styled.div`
+  display: flex;
+  border: 1px solid black;
+  border-radius: 5px;
+  margin: 5px 0;
+  width: 100%;
+`
+
+const CategoryLeftDiv = styled.div`
+  flex: 11;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const CategoryRightDiv = styled.div`
+  flex: 1;
+  display: flex;
+`
+
+const DeleteIcon = styled(Icon)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  font-size: 1rem;
+  width: 100%;
+  height: 100%;
+  color: black;
+  background-color: rgba(255, 0, 0, 0.596);
+  height: 50px;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+
+  &:hover {
+    background-color: red;
+  }
+`
 export default Category;
